a3e0a927d3074100e71dfaf93f534aa7f4a141df
Simon <s.b4rth3l@gmail.com>
2016-09-19 23:02:12 +0200

    Added 'css' and 'img' to valid request types

--- static_resources/app.py
+++ static_resources/app.py
@@ -8,7 +8,7 @@ def handler(event, context):
         raise Exception("Bad Request: no 'request_type' submitted")
 
     request_type = event['request_type']
-    if request_type not in {'js', 'html'}:
+    if request_type not in {'js', 'html', 'css', 'img'}:
         raise Exception("Bad Request: request type '%s' is not valid" % (request_type, ))
 
     requested_file = event['requested_file']
