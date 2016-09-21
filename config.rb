1cb80a4a184bc6c824bab7b6be7c6ca2f49a9d77
Enrico Lummitsch <enrico.lummitsch@ameno.de>
2016-09-21 16:06:49 +0200

    + add base Grid System

---
+++ config.rb
@@ -0,0 +1,9 @@
+
+css_dir = "static_resources/static/css" # by Compass.app 
+sass_dir = "custom/playback-theme" # by Compass.app 
+images_dir = "static_resources/static/img" # by Compass.app 
+output_style = :compressed # by Compass.app 
+relative_assets = true # by Compass.app 
+line_comments = false # by Compass.app 
+sass_options = {:debug_info=>false} # by Compass.app 
+sourcemap = true # by Compass.app 
